06ad72bd0ab997e353f81e3b51d40a82c978905f
Pranav <pranav.suryadevara11@gmail.com>
2023-02-19 00:20:16 +0530

    The RxJs forkJoin Operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,7 +22,7 @@ import {
   throttle,
   throttleTime,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, interval } from "rxjs";
+import { merge, fromEvent, Observable, concat, interval, forkJoin } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { debug, RxJsLoggingLevel, setRxJsLoggingLevel } from "../common/debug";
@@ -45,12 +45,24 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
 
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
-      debug(RxJsLoggingLevel.INFO, "course value ")
-    );
+    // this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
+    //   debug(RxJsLoggingLevel.INFO, "course value ")
+    // );
 
-    // setRxJsLoggingLevel(RxJsLoggingLevel.DEBUG);
-    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+    // // setRxJsLoggingLevel(RxJsLoggingLevel.DEBUG);
+    // setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const lessons$ = this.loadLessons();
+
+    forkJoin(course$, lessons$)
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log("course ", course);
+          console.log("lessons ", lessons);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
