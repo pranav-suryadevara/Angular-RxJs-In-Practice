2338f3623fecbf097f60940afc55d91e661816e2
Pranav <pranav.suryadevara11@gmail.com>
2023-02-18 21:33:55 +0530

    Catch and Rethrow RxJs Error Handling Strategy and the finalize Operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "../model/course";
-import { interval, noop, Observable, of, timer } from "rxjs";
+import { interval, noop, Observable, of, throwError, timer } from "rxjs";
 import {
   catchError,
   delayWhen,
+  finalize,
   map,
   retryWhen,
   shareReplay,
@@ -24,25 +25,16 @@ export class HomeComponent implements OnInit {
     const http$ = createHttpObservable("/api/courses");
 
     const courses$: Observable<Course[]> = http$.pipe(
+      catchError((err) => {
+        console.log("Error Occurred ", err);
+        return throwError(err);
+      }),
+      finalize(() => {
+        console.log("Finalized Executed");
+      }),
       tap(() => console.log("HTTP request executed.")),
       map((res) => Object.values(res["payload"])),
-      shareReplay(),
-      catchError((err) =>
-        of([
-          {
-            id: 0,
-            description: "RxJs In Practice Course",
-            iconUrl:
-              "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
-            courseListIcon:
-              "https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png",
-            longDescription:
-              "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
-            category: "BEGINNER",
-            lessonsCount: 10,
-          },
-        ])
-      )
+      shareReplay()
     );
 
     this.beginnerCourses$ = courses$.pipe(
@@ -57,12 +49,12 @@ export class HomeComponent implements OnInit {
       )
     );
 
-    courses$.subscribe(
-      (courses) => {
-        console.log(courses);
-      },
-      noop, // () => {},  we can use noop instead of empty callback
-      () => console.log("completed")
-    );
+    // courses$.subscribe(
+    //   (courses) => {
+    //     console.log(courses);
+    //   },
+    //   noop, // () => {},  we can use noop instead of empty callback
+    //   () => console.log("completed")
+    // );
   }
 }
